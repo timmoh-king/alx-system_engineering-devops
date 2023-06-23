5dbc364931011b0546ba09cf952f1722be75c023
timmoh-king <timothykariuki43@gmail.com>
2023-06-23 15:23:45 +0300

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file {"school":
-	ensure => 'present'
+	ensure => 'present',
 	path => '/tmp/school',
 	owner => 'www-data',
 	group => 'www-data',
-	mode => 0744,
+	mode => '0744',
 	content => 'I love Puppet'
 }
