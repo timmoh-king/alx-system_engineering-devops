2eeb8d83983e1f45c7c4f45c625d41809e403cf9
timmoh-king <timothykariuki43@gmail.com>
2023-07-26 11:18:46 +0300

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    A python script that uses REST API for a given employee ID
+    returns his/her TODO list progress
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
