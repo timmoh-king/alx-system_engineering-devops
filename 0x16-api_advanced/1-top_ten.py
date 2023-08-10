8185d5c4c0fe9e1bcba3cca33db46d0b81ad9f4e
timmoh-king <timothykariuki43@gmail.com>
2023-08-10 17:21:38 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+    Write a function that queries the Reddit API
+    prints the titles of the first 10 hot posts
+    listed for a given subreddit
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    about_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    user_agent = {'User-Agent': 'MyRedditScript/1.0'}
+
+    res = requests.get(about_url, headers=user_agent)
+
+    if res.status_code == 200:
+        try:
+            data = res.json()
+            posts = data['data']['children']
+            for post in posts[:10]:
+                print(post['data']['title'])
+        except (KeyError, ValueError):
+            print(None)
+    else:
+        print(None)
