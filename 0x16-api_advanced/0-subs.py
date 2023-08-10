67019d21652e9a6e6daf75531a70473ada194d75
timmoh-king <timothykariuki43@gmail.com>
2023-08-10 16:58:41 +0300

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+
+"""
+    Prototype: def number_of_subscribers(subreddit)
+    If not a valid subreddit, return 0
+    NOTE: Invalid subreddits may return a redirect to search results
+    Ensure that you are not following redirects.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """A function that queries the reddit api"""
+    about_url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    user_agent = {'User-Agent': 'MyRedditScript/1.'}
+
+    res = requests.get(about_url, headers=user_agent)
+
+    if res.status_code == 200:
+        try:
+            data = res.json()
+            return data['data']['subscribers']
+        except (KeyError, ValueError):
+            return 0
+    else:
+        return 0
